30f66ad94ca8192eb05ac532d33c90abb2bf2c49
wiint <wintphyopaing.p@gmail.com>
2025-02-20 21:57:12 +0630

    Complete grep

--- ch4/grep.c
+++ ch4/grep.c
@@ -9,10 +9,12 @@ char pattern[] = "ould"; // pattern to search for
 int main() {
     char line[MAXLINE];
 
-    // while (get_line(line, MAXLINE) > 0) {
-    //     printf("%s", line);
-    // }
-    int c = get_line(line, MAXLINE);
+    while (get_line(line, MAXLINE) > 0) {
+        if (strindex(line, pattern) > 0) {
+            printf("\n");
+            printf("Pattern match found: %s\n", line);
+        }
+    }
 
     return 0;
 }
@@ -31,9 +33,16 @@ int get_line(char line[], int max) {
     return i;
 }
 
+/* returns the index of t in s, -1 if none */
 int strindex(char s[], char t[]) {
-    // returns the index of t in s, -1 if none
-    for (int i = 0; s[i] != '\0'; i++) {
+    int i, j, k;
 
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++) {
+        }
+        if (k > 0 && t[k] != '\0') {
+            return i;
+        }
     }
+    return -1;
 }
